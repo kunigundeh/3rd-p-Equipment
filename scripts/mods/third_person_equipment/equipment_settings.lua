a7f4742e963d014418d36caf6e74bbaa83511dc8
dalokraff <87997987+dalokraff@users.noreply.github.com>
2023-03-13 11:24:03 -0500

    add support for handed and non-handed data

--- scripts/mods/third_person_equipment/equipment_settings.lua
+++ scripts/mods/third_person_equipment/equipment_settings.lua
@@ -21,16 +21,20 @@ table structure is like so:
 --example of using this data structure to assign offsets to a location to frag grenades on the default merc mesh
 local new_items = {
     ["units/beings/player/empire_soldier_mercenary/third_person_base/chr_third_person_mesh"] = {
+        --handed example
         ["units/weapons/player/wpn_emp_grenade_01_t1/wpn_emp_grenade_01_t1_3p"] = {
-            attachement_nodes = {
-                {
-                    target = 0, 
-                    source = "a_unwielded_2h" 
+            right = {
+                attachement_nodes = {
+                    {
+                        target = 0, 
+                        source = "a_unwielded_2h" 
+                    },
                 },
+                offset = {0.2,0.2,-0.1},
+                angle = {1.5,0,0.7},
             },
-            offset = {0.2,0.2,-0.1},
-            angle = {1.5,0,0.7},
         },
+        --non handed example
         ["units/weapons/player/wpn_emp_grenade_01_t2/wpn_emp_grenade_01_t2_3p"] = {
             attachement_nodes = {
                 {
@@ -39,7 +43,7 @@ local new_items = {
                 },
             },
             offset = {0.2,0.2,-0.1},
-            angle = {0.5,0,2},
+            angle = {0.5,0,2},          
         },
     },
 }
