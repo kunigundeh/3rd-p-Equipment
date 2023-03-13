ed73cfee7c0bd1a59f9d14702f4ecdb0de7e6b0d
dalokraff <87997987+dalokraff@users.noreply.github.com>
2023-03-13 11:30:53 -0500

    cleanup extraneous files

--- scripts/mods/third_person_equipment/third_person_equipment.lua
+++ scripts/mods/third_person_equipment/third_person_equipment.lua
@@ -8,12 +8,7 @@ local mod = get_mod("third_person_equipment")
 --]]
 
 mod:dofile("scripts/mods/third_person_equipment/hooks")
--- Load definitions
-mod:dofile("scripts/mods/third_person_equipment/third_person_equipment_def")
--- Load extension
 mod:dofile("scripts/mods/third_person_equipment/third_person_equipment_ext")
-
-
 mod:dofile("scripts/mods/third_person_equipment/trinket_settings")
 mod:dofile("scripts/mods/third_person_equipment/equipment_settings")
 
