2ef8e821a1ae97103064442eae20daa1561f1be5
kunigundeh <87445149+kunigundeh@users.noreply.github.com>
2023-03-16 23:26:17 +0100

    placements & scaling fix

--- scripts/mods/third_person_equipment/definitions/dwarf_ranger.lua
+++ scripts/mods/third_person_equipment/definitions/dwarf_ranger.lua
@@ -1,50 +1,113 @@
 --config file for Ranger Veteran's VT1 skin
-local tisch = {
-    ["units/beings/player/dwarf_ranger/third_person_base/chr_third_person_mesh"] = {
-        dr_handgun = {
-          right = {
-            scale = 1,
-            offset = {
-              0.003000000026077,
-              0.003000000026077,
-              0.22699999809265
-            },
-            angle = {
-              3.1419999599457,
-              -1.6039999723434,
-              0.46099999547005
-            }
-          }
+local tisch = {["units/beings/player/dwarf_ranger/third_person_base/chr_third_person_mesh"] = {
+      potion_speed_boost_01 = {
+        offset = {
+          0,
+          0,
+          0
         },
-        dr_1h_axe_shield = {
-          right = {
-            scale = 1,
-            offset = {
-              0.21299999952316,
-              -0.16400000452995,
-              -0.28999999165535
-            },
-            angle = {
-              1.1200000047684,
-              -1.7359999418259,
-              -1.1200000047684
-            }
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      },
+      grenade_fire_01 = {
+        right = {
+          offset = {
+            0,
+            0,
+            0
           },
-          left = {
-            scale = 1,
-            offset = {
-              -0.2339999973774,
-              0.093999996781349,
-              0.25499999523163
-            },
-            angle = {
-              1.7359999418259,
-              1.5599999427795,
-              -0.54900002479553
+          angle = {
+            0,
+            0,
+            0
+          },
+          scale = 1
+        },
+        left = {
+          offset = {
+            0,
+            0,
+            0
+          },
+          angle = {
+            0,
+            0,
+            0
+          },
+          scale = 1
+        }
+      },
+      wpn_side_objective_tome_01 = {
+        left = {
+          offset = {
+            0,
+            0.11299999803305,
+            0.11599999666214
+          },
+          angle = {
+            0.76399999856949,
+            0.99099999666214,
+            -0.78500002622604
+          },
+          
+        }
+      },
+      dr_2h_hammer = {
+        right = {
+          scale = 1,
+          offset = {
+            -0.29300001263618,
+            -0.22100000083447,
+            0.01799999922514
+          },
+          angle = {
+            1.6699999570847,
+            -1.2170000076294,
+            -0.16799999773502
+          },
+          attachement_nodes = {
+            {
+              source = "j_spine",
+              target = 0
             }
           }
         }
       },
-}
+      dr_steam_pistol = {
+        right = {
+          offset = {
+            0.15800000727177,
+            0.17599999904633,
+            -0.24099999666214
+          },
+          angle = {
+            1.9529999494553,
+            0.87699997425079,
+            -1.2339999675751
+          },
+          scale = 1
+        }
+      },
+      dr_1h_axes = {
+        right = {
+          offset = {
+            -0.24799999594688,
+            -0.059000000357628,
+            -0.0089999996125698
+          },
+          angle = {
+            1.6130000352859,
+            -0.14200000464916,
+            -0.28000000119209
+          },
+          scale = 0.87
+        }
+      }
+    }
+  }
 
 return tisch
---
+++ scripts/mods/third_person_equipment/definitions/empire_soldier_mercenary_skin_01.lua
@@ -0,0 +1,37 @@
+-- config file for Mercenary's Flamboyant Sellsword skin
+
+local tisch = {["units/beings/player/empire_soldier_mercenary_skin_01/third_person_base/chr_third_person_mesh"] = {
+    es_repeating_handgun = {
+      right = {
+        scale = 1,
+        angle = {
+          -3.0539999008179,
+          0.10999999940395,
+          1.0759999752045
+        },
+        offset = {
+          0.003000000026077,
+          0.24799999594688,
+          0.32499998807907
+        }
+      }
+    },
+    es_2h_sword = {
+      right = {
+        scale = 1,
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        }
+      }
+    }
+  }
+}
+
+return tisch
