d2046cc2e508477bded2b5deca6f859f04aac627
kunigundeh <87445149+kunigundeh@users.noreply.github.com>
2023-06-26 22:10:49 +0200

    Create dwarf_ranger_ironbreaker.lua

---
+++ scripts/mods/third_person_equipment/definitions/dwarf_ranger_ironbreaker.lua
@@ -0,0 +1,64 @@
+-- config file for Bardin Ironbreaker's base skin
+
+
+local tisch = {["units/beings/player/dwarf_ranger_ironbreaker/third_person_base/chr_third_person_mesh"] = {
+    dr_drakefire_pistols = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      },
+      left = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    dr_1h_axe_shield = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      },
+      left = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    }
+  }
+}
+
+return tisch
