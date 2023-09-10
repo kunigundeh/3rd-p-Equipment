9045fd787105987699185272770a7db04fdd378a
kunigundeh <87445149+kunigundeh@users.noreply.github.com>
2023-09-10 23:15:05 +0200

    more zealot

---
+++ scripts/mods/third_person_equipment/definitions/witch_hunter_warrior_priest_skin_01.lua
@@ -0,0 +1,64 @@
+-- config file for Saltzpyre Warrior Priest's Prophet Skin
+local tisch = {
+    ["units/beings/player/witch_hunter_warrior_priest_skin_01/third_person_base/chr_third_person_mesh"] = 
+{
+    wh_hammer_book = {
+      right = {
+        offset = {
+          0.10400000214577,
+          0.25,
+          -0.046000000089407
+        },
+        angle = {
+          1.1779999732971,
+          -2.029000043869,
+          -1.7810000181198
+        },
+        scale = 0.89
+      },
+      left = {
+        offset = {
+          -0.0099999997764826,
+          -0.0099999997764826,
+          -0.10800000280142
+        },
+        angle = {
+          -0.39300000667572,
+          -0.26199999451637,
+          -0.22699999809265
+        },
+        scale = 0.86
+      }
+    },
+    wh_flail_shield = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      },
+      left = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    }
+  }
+}
+
+return tisch
