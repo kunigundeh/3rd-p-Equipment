7552cd5352e6c8dd68f86cbfc757f496847b5211
dalokraff <87997987+dalokraff@users.noreply.github.com>
2024-11-03 21:21:53 -0600

    gk skins

--- scripts/mods/third_person_equipment/definitions/empire_soldier_breton.lua
+++ scripts/mods/third_person_equipment/definitions/empire_soldier_breton.lua
@@ -1,7 +1,186 @@
 -- config file for Kruber's Grailknight Baseskin
 
-local tisch = { ["units/beings/player/empire_soldier_breton/third_person_base/chr_third_person_mesh"] = 
-{
+local tisch = { ["units/beings/player/empire_soldier_breton/third_person_base/chr_third_person_mesh"] =
+  {
+    es_1h_mace = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    healthkit_first_aid_kit_01 = {
+      left = {
+        angle = {
+          -1.6039999723434,
+          -0.33399999141693,
+          -2.6740000247955
+        },
+        offset = {
+          0.1059999987483,
+          -0.1059999987483,
+          -0.17000000178814
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        },
+
+      }
+    },
+    potion_healing_draught_01 = {
+      left = {
+        angle = {
+          1.3370000123978,
+          0.33399999141693,
+          -0.60199999809265
+        },
+        offset = {
+          0.1169999986887,
+          -0.14900000393391,
+          -0.19099999964237
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      }
+    },
+    potion_damage_boost_01 = {
+      angle = {
+        1.8049999475479,
+        -0.33399999141693,
+        0.40099999308586
+      },
+      offset = {
+        0.17000000178814,
+        0.20200000703335,
+        -0.1169999986887
+      },
+      attachement_nodes = {
+        {
+          source = "j_spine",
+          target = 0
+        }
+      },
+
+    },
+    potion_cooldown_reduction_01 = {
+      angle = {
+        1.8049999475479,
+        -0.33399999141693,
+        0.40099999308586
+      },
+      offset = {
+        0.17000000178814,
+        0.20200000703335,
+        -0.1169999986887
+      },
+      attachement_nodes = {
+        {
+          source = "j_spine",
+          target = 0
+        }
+      },
+
+    },
+    potion_speed_boost_01 = {
+      angle = {
+        1.8049999475479,
+        -0.33399999141693,
+        0.40099999308586
+      },
+      offset = {
+        0.17000000178814,
+        0.20200000703335,
+        -0.1169999986887
+      },
+      attachement_nodes = {
+        {
+          source = "j_spine",
+          target = 0
+        }
+      },
+
+    },
+    wpn_side_objective_tome_01 = {
+      left = {
+        angle = {
+          1.5369999408722,
+          0.53500002622604,
+          -0.33399999141693
+        },
+        offset = {
+          0.1059999987483,
+          -0.12800000607967,
+          -0.19099999964237
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    es_2h_sword = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    es_bastard_sword = {
+      right = {
+        offset = {
+          0.0060000000521541,
+          0.026000000536442,
+          0.179000005126
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    es_2h_war_hammer = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
     es_1h_sword_shield_breton = {
       right = {
         offset = {
@@ -30,6 +209,333 @@ local tisch = { ["units/beings/player/empire_soldier_breton/third_person_base/ch
         scale = 1
       }
     },
+    grenade_frag_02 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    grenade_frag_01 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    grenade_fire_01 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    grenade_fire_02 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    wpn_grimoire_01 = {
+      angle = {
+        1.470999956131,
+        -1.470999956131,
+        0.20100000500679
+      },
+      offset = {
+        0.064000003039837,
+        0.043000001460314,
+        -0.24500000476837
+      },
+      attachement_nodes = {
+        {
+          source = "j_hips",
+          target = 0
+        }
+      },
+
+    }
+  },
+  ["units/beings/player/empire_soldier_breton_skin_01/third_person_base/chr_third_person_mesh"] =
+  {
+    es_1h_mace = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    healthkit_first_aid_kit_01 = {
+      left = {
+        angle = {
+          -1.6039999723434,
+          -0.33399999141693,
+          -2.6740000247955
+        },
+        offset = {
+          0.1059999987483,
+          -0.1059999987483,
+          -0.17000000178814
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        },
+
+      }
+    },
+    potion_healing_draught_01 = {
+      left = {
+        angle = {
+          1.3370000123978,
+          0.33399999141693,
+          -0.60199999809265
+        },
+        offset = {
+          0.1169999986887,
+          -0.14900000393391,
+          -0.19099999964237
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      }
+    },
+    potion_damage_boost_01 = {
+      angle = {
+        1.8049999475479,
+        -0.33399999141693,
+        0.40099999308586
+      },
+      offset = {
+        0.17000000178814,
+        0.20200000703335,
+        -0.1169999986887
+      },
+      attachement_nodes = {
+        {
+          source = "j_spine",
+          target = 0
+        }
+      },
+
+    },
+    potion_cooldown_reduction_01 = {
+      angle = {
+        1.8049999475479,
+        -0.33399999141693,
+        0.40099999308586
+      },
+      offset = {
+        0.17000000178814,
+        0.20200000703335,
+        -0.1169999986887
+      },
+      attachement_nodes = {
+        {
+          source = "j_spine",
+          target = 0
+        }
+      },
+
+    },
+    potion_speed_boost_01 = {
+      angle = {
+        1.8049999475479,
+        -0.33399999141693,
+        0.40099999308586
+      },
+      offset = {
+        0.17000000178814,
+        0.20200000703335,
+        -0.1169999986887
+      },
+      attachement_nodes = {
+        {
+          source = "j_spine",
+          target = 0
+        }
+      },
+
+    },
+    wpn_side_objective_tome_01 = {
+      left = {
+        angle = {
+          1.5369999408722,
+          0.53500002622604,
+          -0.33399999141693
+        },
+        offset = {
+          0.1059999987483,
+          -0.12800000607967,
+          -0.19099999964237
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    es_2h_sword = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
     es_bastard_sword = {
       right = {
         offset = {
@@ -44,8 +550,226 @@ local tisch = { ["units/beings/player/empire_soldier_breton/third_person_base/ch
         },
         scale = 1
       }
+    },
+    es_2h_war_hammer = {
+      right = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    es_1h_sword_shield_breton = {
+      right = {
+        offset = {
+          0.039000000804663,
+          0.032000001519918,
+          -0.1089999973774
+        },
+        angle = {
+          -0.1630000025034,
+          0.49000000953674,
+          0
+        },
+        scale = 0.86
+      },
+      left = {
+        offset = {
+          0,
+          0,
+          0
+        },
+        angle = {
+          0,
+          0,
+          0
+        },
+        scale = 1
+      }
+    },
+    grenade_frag_02 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    grenade_frag_01 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    grenade_fire_01 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    grenade_fire_02 = {
+      right = {
+        angle = {
+          1.7380000352859,
+          0,
+          0.40099999308586
+        },
+        offset = {
+          0.064000003039837,
+          0.2339999973774,
+          0.14900000393391
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+      left = {
+        angle = {
+          0,
+          0,
+          0
+        },
+        offset = {
+          0,
+          0,
+          0
+        },
+        attachement_nodes = {
+          {
+            source = "j_hips",
+            target = 0
+          }
+        }
+      },
+
+    },
+    wpn_grimoire_01 = {
+      angle = {
+        1.470999956131,
+        -1.470999956131,
+        0.20100000500679
+      },
+      offset = {
+        0.064000003039837,
+        0.043000001460314,
+        -0.24500000476837
+      },
+      attachement_nodes = {
+        {
+          source = "j_hips",
+          target = 0
+        }
+      },
+
     }
-  }
+  },
 }
 
 return tisch
